7a04d5ba3eba1cd197482e80c2775ef7c5da04f3
AlexAirCG <108254394+AlexAirCG@users.noreply.github.com>
2023-11-13 06:52:00 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,52 +1,67 @@
-const minutesBlock = document.querySelector('.js-minutes'),
+const daysBlock = document.querySelector('.js-days'),
+	hoursBlock = document.querySelector('.js-hours'),
+	minutesBlock = document.querySelector('.js-minutes'),
 	secondsBlock = document.querySelector('.js-seconds'),
-	hundredthsSecondBlock = document.querySelector('.js-hundredths-second'),
 	btnStart = document.querySelector('.js-btn-start'),
 	btnStop = document.querySelector('.js-btn-stop'),
 	btnReset = document.querySelector('.js-btn-reset');
 
 let interval;
+let days = 0;
+let hours = 0;
 let minutes = 0;
 let seconds = 0;
-let miliseconds = 0;
+
+
 
 const startTimer = () => {
-	// miliseconds++;
-	miliseconds += 10;
-
-	if (miliseconds <= 99) {
-		hundredthsSecondBlock.innerHTML = miliseconds;
-	}
-
-	if (miliseconds == 100) {
-		hundredthsSecondBlock.innerHTML = '00';
-	}
-
-	if (miliseconds > 99) {
-		seconds++;
-		secondsBlock.innerHTML = '0' + seconds;
-		miliseconds = 0;
-	}
+	seconds++;
+	secondsBlock.innerHTML = '0' + seconds;
 
 	if (seconds > 9) {
 		secondsBlock.innerHTML = seconds;
+	}
+
+	if (seconds == 60) {
+		secondsBlock.innerHTML = '00';
 	}
 
 	if (seconds > 59) {
 		minutes++;
 		minutesBlock.innerHTML = '0' + minutes;
 		seconds = 0;
-		secondsBlock.innerHTML = '0' + seconds;
 	}
 
 	if (minutes > 9) {
 		minutesBlock.innerHTML = minutes;
 	}
+
+	if (minutes > 59) {
+		hours++;
+		hoursBlock.innerHTML = '0' + hours;
+		minutes = 0;
+		minutesBlock.innerHTML = '0' + minutes;
+	}
+
+	if (hours > 9) {
+		hoursBlock.innerHTML = hours;
+	}
+
+	if (hours > 23) {
+		days++;
+		daysBlock.innerHTML = '0' + days;
+		hours = 0;
+		hoursBlock.innerHTML = '0' + hours;
+	}
+
+	if (days > 9) {
+		daysBlock.innerHTML = days;
+	}
 }
 
 btnStart.addEventListener('click', () => {
 	clearInterval(interval);
-	interval = setInterval(startTimer, 10);
+	interval = setInterval(startTimer, 1000);
 });
 
 btnStop.addEventListener('click', () => {
@@ -56,11 +71,13 @@ btnStop.addEventListener('click', () => {
 btnReset.addEventListener('click', () => {
 	clearInterval(interval);
 	seconds = 0;
-	miliseconds = 0;
 	minutes = 0;
+	hours = 0;
+	days = 0;
 
-	minutesBlock.innerHTML = '00';
 	secondsBlock.innerHTML = '00';
-	hundredthsSecondBlock.innerHTML = '00';
+	minutesBlock.innerHTML = '00';
+	hoursBlock.innerHTML = '00';
+	daysBlock.innerHTML = '00';
 });
 
